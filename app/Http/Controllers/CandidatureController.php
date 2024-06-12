13af396da36bb905787196db0cdea9d7a63b8a8b
ndongba <ndongba01@gmail.com>
2024-06-12 19:24:40 +0000

    finition de formulaire de candidature

--- app/Http/Controllers/CandidatureController.php
+++ app/Http/Controllers/CandidatureController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CandidatureController extends Controller
 {
-    //
+    public function afficher_formulaire(){
+        
+        return view('candidature');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\CandidatureController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('candidature',[CandidatureController::class,'afficher_formulaire']);
