3cd09946fdf1919df80bec9096573f44594fbadb
Adama Dabo <dabodabo9064@gmail.com>
2024-06-13 02:59:36 +0000

    Recuperations des formations disponibles dans la base de donnée et les afficher pour les candidatures

--- app/Http/Controllers/FormationController.php
+++ app/Http/Controllers/FormationController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Formation;
 
 use Illuminate\Http\Request;
 
 class FormationController extends Controller
 {
-    //
+    //Ctte fonction c'est pour l'affichage des formations
+    public function index()
+    {
+        // Récupérer toutes les formations
+        $formations = Formation::all();
+
+        // Passer les formations à la vue
+        return view('formation', compact('formations'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+//Cette route va nous permettre de voir l'ensemble des formations
+Route::get('/formations', [FormationController::class, 'index'])->name('formations');
