7363e6b90a62ae63e1f589c69592572f5245ee02
ndongba <ndongba01@gmail.com>
2024-06-14 14:52:17 +0000

    stylisation de la page candidature

--- resources/views/affiche_candidature.blade.php
+++ resources/views/affiche_candidature.blade.php
@@ -5,14 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Affiche Candidature</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/candidature.css') }}">
   </head>
   <body>
    
     <div class="row">
-    <div>
-            
-            <title>{{ $user->prenom}}</title>
-            <title>{{ $user->nom}}</title>
+      <div class="infos_perso">
+      <div>
+            <img src="https://img.freepik.com/photos-gratuite/portrait-homme-afro-americain_23-2149072179.jpg?size=626&ext=jpg&ga=GA1.1.1354621366.1718124233&semt=ais_user" alt="">
+            <title>Adama</title>
+            <title>Dabo</title>
         </div>
         <div class="infos">
             <div>
@@ -23,16 +25,16 @@
             </div>
             <div>
                 <h4>Contact</h4>
-                <p>{{ $user->telephone}}</p>
-                <p>{{ $user->email}}</p>
+                <p>Tél:770000000</p>
+                <p>email:dabodabo@gmail.com</p>
             </div>
-            </div>
-        
-            <h2>Status:{{ $data->status}}</h2>
+          </div>
+        </div>
+            <h2>Status: En evaluation</h2>
             <h3>Biographie</h3>
-            <p> {{ $data->biographie}}</p>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
             <h3>Motivation</h3>
-            <p>{{ $data->motivation}}</p>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
        
     </div>
     </body>
--- resources/views/candidature.blade.php
+++ resources/views/candidature.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> 
+    
+    <link rel="stylesheet" href="{{ asset('css/candidature.css') }}">
+
     <title>Formulaire de Candidature</title>
 </head>
 <body>
+
+    <h3 id="titre1">Formulaire de Candidature</h3>
+    <p id="p1">Formation Développeur.euse Full Stack</p>
     
     <form action="candidature" method="POST" class="container" enctype="multipart/form-data">
         @csrf
@@ -54,7 +60,7 @@
             <input type="file" class="form-control" id="exampleFormControlInput1" name="cv">
         </div> 
 
-            <button type="submit" class="btn btn-danger">Envoyer</button>
+            <button type="submit" id="bouton">Envoyer</button>
 </form>
 </body>
 </html>
